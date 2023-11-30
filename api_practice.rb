7277f44c4da5e1a07752e29227ee92987e45048f
jmbehlmann <110798325+jmbehlmann@users.noreply.github.com>
2023-11-29 20:28:13 -0600

    change program to count number of hashes in api

--- api_practice.rb
+++ api_practice.rb
@@ -2,12 +2,16 @@ require "http"
 
 # https://data.cityofchicago.org/resource/xzkq-xp2w.json
 
-info = HTTP.get("https://data.cityofchicago.org/resource/xzkq-xp2w.json").parse
+# https://data.cityofchicago.org/resource/n4bx-5kf6.json
+
+info = HTTP.get("https://data.cityofchicago.org/resource/n4bx-5kf6.json").parse
 
 job_titles = []
 info.each_with_index do |job, index|
   job_titles << info[index]["job_titles"]
 end
+
+p job_titles.length
 
 hourly_rate = []
 info.each_with_index do |rate, index|
@@ -19,9 +23,14 @@ info.each_with_index do |salary, index|
   annual_salary << info[index]["annual_salary"]
 end
 
+# p annual_salary[0].is_a?(String)
+
+salaries = []
 annual_salary.each do |salary|
-  if salary.type ==
+  if salary.is_a?(String)
+    salaries << salary.to_i
+  end
 end
-p annual_salary.sort
+p salaries.length
 
 # p info
